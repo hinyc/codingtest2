dc0cc2250a1b85f1c1748330b524841e2b4f2627
hinyc <dlsduf@gmail.com>
2021-08-26 22:24:08 +0900

    문법 수정

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 //프로그래머스 코테연습 약수의 개수와 덧셈
-let left = 13;
-let right = 17;
-//result 43
 
 function solution(left, right) {
   var answer = 0;
 
   //left rigtht 까지 의 수들의 약수를 구한다.
 
-  for (i = left; i <= right; i += 1) {
+  for (let i = left; i <= right; i += 1) {
     let divisor = [];
-    for (j = 1; j <= right; j += 1) {
+    for (let j = 1; j <= right; j += 1) {
       if (i % j === 0) {
         divisor.push(j);
         //j는 약수
@@ -31,4 +28,5 @@ function solution(left, right) {
   //합을 반환한다.
   return answer;
 }
-solution(13, 17);
+
+solution(24, 27);
